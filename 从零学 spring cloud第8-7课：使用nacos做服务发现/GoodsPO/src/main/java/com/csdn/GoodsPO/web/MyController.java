65066b874dd247445db7719391b791804bb5daeb
heyu52 <49085948+heyu52@users.noreply.github.com>
2019-09-07 16:15:10 +0800

    Add files via upload

---
+++ 从零学 spring cloud第8-7课：使用nacos做服务发现/GoodsPO/src/main/java/com/csdn/GoodsPO/web/MyController.java
@@ -0,0 +1,24 @@
+package com.csdn.GoodsPO.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MyController {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public MyController(RestTemplate restTemplate) {this.restTemplate = restTemplate;}
+
+    @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
+    public String echo(@PathVariable String str) {
+        return restTemplate.getForObject("http://GoodsPR/echo/" + str, String.class);
+    }
+}
